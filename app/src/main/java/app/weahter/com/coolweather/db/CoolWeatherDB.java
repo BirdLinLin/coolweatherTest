bed45a88205e3b8c10d01889d7d332fc2ca5d054
wanglin <wanglin@sagereal.com>
2017-09-26 09:01:03 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/app/weahter/com/coolweather/db/CoolWeatherDB.java
@@ -0,0 +1,93 @@
+package app.weahter.com.coolweather.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.weahter.com.coolweather.model.City;
+import app.weahter.com.coolweather.model.Province;
+
+/**
+ * Created by wanglin on 9/23/17.
+ */
+
+public class CoolWeatherDB {
+    //数据库名
+    public static final String DB_NAME = "cool_weather";
+    //数据库版本
+    public static final int VERSION = 1;
+    private static CoolWeatherDB coolWeatherDB;
+    private SQLiteDatabase db;
+    private CoolWeatherDB(Context context){
+        CoolWeatherOpenHelper dbHelper = new CoolWeatherOpenHelper(context,DB_NAME,null,VERSION);
+        db =dbHelper.getWritableDatabase();
+    }
+    //获取CoolWeatherDB实例
+    public synchronized  static CoolWeatherDB getInstance(Context context){
+        if(coolWeatherDB == null){
+            CoolWeatherDB coolWeatherDB = new CoolWeatherDB(context);
+        }
+        return coolWeatherDB;
+    }
+    //将省份信息存入数据库
+    public void saveProvince(Province province){
+        if(province != null){
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("province_name",province.getProvinceName());
+            contentValues.put("province_code",province.getProvinceCode());
+            db.insert("Province",null,contentValues);
+        }
+
+    }
+    //从数据可中读取省份信息
+    public List<Province> loadProvince(){
+        List<Province> list = new ArrayList<Province>();
+        Cursor cursor = db.query("Province",null,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                Province province = new Province();
+                province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+                province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+                list.add(province);
+            }while(cursor.moveToNext());
+
+        }
+        return list;
+    }
+    //将城市的信息存入数据库中
+    public void saveCity(City city){
+        if(city != null){
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("city_name",city.getCityName());
+            contentValues.put("city_code",city.getCityCode());
+            contentValues.put("province_id",city.getProvinceId());
+            db.insert("City",null,contentValues);
+        }
+    }
+    //读取数据库中的城市信息
+    public List<City> loadCity(int provinceId){
+        List<City> list = new ArrayList<City>();
+        Cursor cursor = db.query("City", null, "province_id = ?",
+                new String[] { String.valueOf(provinceId) }, null, null, null);
+        if(cursor.moveToFirst()){
+            do{
+                City city = new City();
+                city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+                city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+                city.setProvinceId(cursor.getInt(cursor.getColumnIndex("province_id")));
+                list.add(city);
+            }while(cursor.moveToNext());
+
+        }
+        return list;
+    }
+    //将县扽信息存入数据库中
+    //从数据库中读取县的信息
+}
---
+++ app/src/main/java/app/weahter/com/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package app.weahter.com.coolweather.model;
+
+/**
+ * Created by wanglin on 9/22/17.
+ */
+
+public class Province {
+    private  int id;
+    private  String provinceName;
+    private  String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
